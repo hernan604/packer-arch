bd72fbf6878819c3a95efc39dbb07777299c4de2
Hernan <hernanlopes@gmail.com>
2015-04-09 19:23:52 +0000

    working config

--- perlbrew.sh
+++ perlbrew.sh
@@ -8,6 +8,7 @@ perlbrew init
 perlbrew -j 4 install 5.21.9
 perlbrew switch 5.21.9
 perlbrew install-cpanm
+cpanm Mojolicious
 cpanm Selenium::Remote::Driver JSON::XS File::Slurp MIME::Base64 Digest::SHA1
 cpanm Config::Any Moo Mojo::Pg SQL::Abstract DDP POE::Component::Server::IRC
 cpanm Mojo::Redis2 Mojolicious DateTime
@@ -15,4 +16,5 @@ mkdir ~/perl
 cd ~/perl
 git clone https://github.com/hernan604/Web-IRC.git ~/perl/
 "
+
 sudo su - vagrant -c "$COMMAND"
--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 echo '==> installation complete!'
-/usr/bin/sleep 3
-/usr/bin/umount ${TARGET_DIR}
-/usr/bin/systemctl reboot

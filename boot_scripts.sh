a28e0fd8e3f8d3ccec7c89297d4659c8f4b44d58
Hernan <hernanlopes@gmail.com>
2015-04-10 15:54:03 +0000

    added boot scripts rc.local compatible

---
+++ boot_scripts.sh
@@ -0,0 +1,27 @@
+SERVICE="rc-local.service"
+FILE="/usr/lib/systemd/system/$SERVICE"
+INIT_SCRIPT="/etc/rc.local"
+
+cat <<COMMANDS > $FILE
+[Unit]
+Description=$INIT_SCRIPT Compatibility
+
+[Service]
+Type=oneshot
+ExecStart=$INIT_SCRIPT
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+COMMANDS
+
+cat <<INIT_SCRIPT_CONTENT > $INIT_SCRIPT
+#!/usr/bin/env bash
+
+echo boot time: \`date\` >> /home/vagrant/debug
+
+exit 0
+INIT_SCRIPT_CONTENT
+
+chmod +x $INIT_SCRIPT
+systemctl enable $SERVICE
